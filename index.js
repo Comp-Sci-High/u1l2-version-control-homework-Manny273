6dbb31e779d3d09d5b3f70ca1dc65b5153d2e1c6
Manny273 <134006013+Manny273@users.noreply.github.com>
2024-09-20 13:16:39 +0000

    last task done

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ console.log(versionControlBenefits);
 
 // 4. Explain the steps to pushing changes in Git.
 // Write your answer as an array of steps below, with each step being a string.
-let stepsToPushChanges = [];
+let stepsToPushChanges = ["do git add", "git git commit -m Message "];
 
 console.log("What are the steps to pushing changes in Git?");
 console.log(stepsToPushChanges); 
